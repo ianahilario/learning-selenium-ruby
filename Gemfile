56c1c6eadfd537c081dfbc6b954849008f99928a
ianahilario <iana.hilario@gmail.com>
2024-06-23 12:51:34 +0800

    add rspec

--- Gemfile
+++ Gemfile
@@ -1,1 +1,5 @@
+source 'https://rubygems.org'
+
 gem 'selenium-devtools', '= 0.125.0'
+gem 'selenium-webdriver', '= 4.21.1'
+gem 'rspec'
---
+++ tests/first_script_rspec.rb
@@ -0,0 +1,27 @@
+require 'selenium-webdriver'
+
+
+describe 'Using Selenium' do
+  before do
+    @driver = Selenium::WebDriver.for :chrome
+  end
+
+  example 'uses eight components' do
+    @driver.get('https://www.selenium.dev/selenium/web/web-form.html')
+
+    title = @driver.title
+    expect(title).to eq('Web form')
+
+    @driver.manage.timeouts.implicit_wait = 500
+
+    text_box = @driver.find_element(name: 'my-text')
+    submit_button = @driver.find_element(tag_name: 'button')
+
+    text_box.send_keys('Selenium')
+    submit_button.click
+
+    message = @driver.find_element(id: 'message')
+    value = message.text
+    expect(value).to eq('Received!')
+  end
+end
